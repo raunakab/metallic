58a52e583b155f7793829e4391a117a666c114db
Raunak Bhagat <rabhagat31@gmail.com>
2024-06-03 00:11:35 -0700

    Add proper error-handling in example/todo-app.rs

--- examples/todo-app.rs
+++ examples/todo-app.rs
@@ -19,35 +19,56 @@ pub struct App {
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let rendering_engine = block_on(new_rendering_engine(event_loop, Color::WHITE)).unwrap();
-        self.rendering_engine = Some(rendering_engine);
+        if let Err(error) = block_on(resume(self, event_loop)) {
+            eprintln!("{:?}", error);
+        };
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
-        if let Some(rendering_engine) = self.rendering_engine.as_mut() {
-            match event {
-                WindowEvent::CloseRequested | WindowEvent::Destroyed => exit(self, event_loop),
-                WindowEvent::Resized(new_size) => {
-                    resize(rendering_engine, new_size);
-                    request_redraw(rendering_engine);
-                }
-                WindowEvent::KeyboardInput { event, .. } => match event.logical_key {
-                    Key::Named(NamedKey::Control) => self.control_element_state = Some(event.state),
-                    Key::Character(c)
-                        if self.control_element_state == Some(ElementState::Pressed)
-                            && c == "w" =>
-                    {
-                        exit(self, event_loop);
-                    }
-                    _ => (),
-                },
-                WindowEvent::RedrawRequested => {
-                    render(rendering_engine).unwrap();
-                }
-                _ => (),
-            }
+        if let Err(error) = handle_window_event(self, event_loop, event) {
+            eprintln!("{:?}", error);
         };
     }
+}
+
+async fn resume(
+    app: &mut App,
+    event_loop: &ActiveEventLoop,
+) -> anyhow::Result<()> {
+    let rendering_engine = new_rendering_engine(event_loop, Color::WHITE).await?;
+    app.rendering_engine = Some(rendering_engine);
+    Ok(())
+}
+
+fn handle_window_event(
+    app: &mut App,
+    event_loop: &ActiveEventLoop,
+    event: WindowEvent,
+) -> anyhow::Result<()> {
+    if let Some(rendering_engine) = app.rendering_engine.as_mut() {
+        match event {
+            WindowEvent::CloseRequested | WindowEvent::Destroyed => exit(app, event_loop),
+            WindowEvent::Resized(new_size) => {
+                resize(rendering_engine, new_size);
+                request_redraw(rendering_engine);
+            }
+            WindowEvent::KeyboardInput { event, .. } => match event.logical_key {
+                Key::Named(NamedKey::Control) => app.control_element_state = Some(event.state),
+                Key::Character(c)
+                    if app.control_element_state == Some(ElementState::Pressed)
+                        && c == "w" =>
+                {
+                    exit(app, event_loop);
+                }
+                _ => (),
+            },
+            WindowEvent::RedrawRequested => {
+                render(rendering_engine).unwrap();
+            }
+            _ => (),
+        }
+    };
+    Ok(())
 }
 
 fn exit(app: &mut App, event_loop: &ActiveEventLoop) {
